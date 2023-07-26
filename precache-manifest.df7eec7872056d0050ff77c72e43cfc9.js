5bd51eb88563cc27d670664367a65ba028400e69
Tanu Goyal <Tanu.Goyal@airtel.com>
2023-07-26 14:39:49 +0530

    Updates

---
+++ precache-manifest.df7eec7872056d0050ff77c72e43cfc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22581d75bc6ff4beab225a2d4d37645",
+    "url": "/paymentFlow/index.html"
+  },
+  {
+    "revision": "d3aff8a9591d992852ea",
+    "url": "/paymentFlow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7d44edebc5da107acf6",
+    "url": "/paymentFlow/static/js/2.f73d1cab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paymentFlow/static/js/2.f73d1cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3aff8a9591d992852ea",
+    "url": "/paymentFlow/static/js/main.3b5bcb12.chunk.js"
+  },
+  {
+    "revision": "9f032a36fd893305342e",
+    "url": "/paymentFlow/static/js/runtime-main.e3264bfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paymentFlow/static/media/logo.5d5d9eef.svg"
+  }
+]);
